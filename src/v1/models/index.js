8cc7ce71632f3d2a2b3f84826f144c506ae918bb
NguyenDuyKhang <nguyenduykhanghuflit@gmail.com>
2024-07-29 12:45:31 +0700

    hide cartdetail

--- src/v1/models/index.js
+++ src/v1/models/index.js
@@ -1,19 +1,19 @@
-const User = require('./user.model');
-const Categories = require('./category.model');
-const Product = require('./product.model');
-const Cart = require('./cart.model');
-const CartDetail = require('./cartDetail.model');
-const Order = require('./order.model');
-const OrderDetail = require('./orderdetail.model');
-const Favorite = require('./favorite.model');
+// const User = require('./user.model');
+// const Categories = require('./category.model');
+// const Product = require('./product.model');
+// const Cart = require('./cart.model');
+// const CartDetail = require('./cartDetail.model');
+// const Order = require('./order.model');
+// const OrderDetail = require('./orderdetail.model');
+// const Favorite = require('./favorite.model');
 
-module.exports = {
-   User,
-   Categories,
-   Product,
-   Cart,
-   CartDetail,
-   Order,
-   OrderDetail,
-   Favorite,
-};
+// module.exports = {
+//    User,
+//    Categories,
+//    Product,
+//    Cart,
+//    CartDetail,
+//    Order,
+//    OrderDetail,
+//    Favorite,
+// };
