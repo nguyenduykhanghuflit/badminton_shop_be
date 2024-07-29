325c86a2b21587930f29bf10be262ddc6faa8838
NguyenDuyKhang <nguyenduykhanghuflit@gmail.com>
2024-07-29 12:46:31 +0700

    delete cart router

--- src/v1/routes/index.router.js
+++ src/v1/routes/index.router.js
@@ -1,13 +1,13 @@
 const categories = require('./categories.router');
 const products = require('./product.router');
 const users = require('./user.router');
-const carts = require('./cart.router');
+// const carts = require('./cart.router');
 // const orders = require('./order.router');
 const initRouter = (app) => {
    app.use('/api/categories', categories);
    app.use('/api/product', products);
    app.use('/api/user', users);
-   app.use('/api/cart', carts);
+   // app.use('/api/cart', carts);
    // app.use('/api/order', orders);
 };
 
